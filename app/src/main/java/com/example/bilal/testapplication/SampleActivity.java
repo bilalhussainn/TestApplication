aa03b47e64624bf694b472fb6c628d89302cd848
bilal Hussain <bilal23.hussain@gmail.com>
2018-03-10 16:10:22 +0530

    Commit 9

--- app/src/main/java/com/example/bilal/testapplication/SampleActivity.java
+++ app/src/main/java/com/example/bilal/testapplication/SampleActivity.java
@@ -2,13 +2,20 @@ package com.example.bilal.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class SampleActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView) TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+
+        textView.setText("Sample Activity");
 
 ///
 
